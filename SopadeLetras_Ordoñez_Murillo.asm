34908e88b6da1c2ddd36c95687d3644f0a744cad
Marck <maanmuri@espol.edu.ec>
2021-06-27 11:12:31 -0500

    archivo de esamblador

---
+++ SopadeLetras_Ordoñez_Murillo.asm
@@ -0,0 +1,20 @@
+
+;Declaracion de variables
+.data 
+var1 db ?      
+var2 db ? 
+var3 db ? 
+
+
+;Declaracion del punto de inicio del programa
+.stack
+.code
+.start up
+
+
+;Inicializacion de los registros
+mov ax,0000h  
+mov bx,0000h
+mov cx,0000h               
+               
+           
