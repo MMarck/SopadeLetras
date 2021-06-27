a4a2e81d55470946540f039374264a912da27e0a
Marck <maanmuri@espol.edu.ec>
2021-06-27 12:41:31 -0500

    menu de inicio

--- SopadeLetras_Ordoñez_Murillo.asm
+++ SopadeLetras_Ordoñez_Murillo.asm
@@ -1,10 +1,17 @@
+;libreria de emu8086 (solo para usar la funcion "printn" )
+include 'emu8086.inc' 
+ORG 100h            
 
-;Declaracion de variables
-.data 
-var1 db ?      
-var2 db ? 
-var3 db ? 
-
+;;;;;;; VARIABLES ;;;;;;;;
+.data
+msgInicio1 db " Por favor ingrese un numero para seleccionar$"
+msgInicio2 db " una de las siguientes categorias:$"
+msgOP1 db " 1 - Animales$"
+msgOP2 db " 2 - Vehiculos de transporte$"
+msgOP3 db " 3 - Lenguajes de programacion$"
+msgSelect db "Ingrese el numero de la categoria deseada: $"
+ 
+;;;;;;; FIN VARIABLES ;;;;;;;;
 
 ;Declaracion del punto de inicio del programa
 .stack
@@ -15,6 +22,72 @@ var3 db ?
 ;Inicializacion de los registros
 mov ax,0000h  
 mov bx,0000h
-mov cx,0000h               
-               
+mov cx,0000h
+mov dx,0000h
+     
+      
            
+;Este bloque de codigo imprime el mensaje de bienvenida y 
+;toma la opcion de categoria para inciar el juego           
+menuInicio: 
+
+CALL nwLine
+printn ' !!      Bienvenido al juego de sopa de letras   !!' 
+
+CALL nwLine
+MOV AH, 09h
+LEA DX,msgInicio1
+INT 21h  
+
+CALL nwLine
+MOV AH, 09h
+LEA DX,msgInicio2
+INT 21h 
+         
+         
+CALL nwLine
+CALL nwLine
+MOV AH, 09h
+LEA DX,msgOP1
+INT 21h  
+
+CALL nwLine
+MOV AH, 09h
+LEA DX,msgOP2
+INT 21h  
+
+CALL nwLine
+MOV AH, 09h
+LEA DX,msgOP3
+INT 21h
+
+;esta partecita imipmer un mensaje y
+;pide el valor para la categoria
+CALL nwLine
+CALL nwLine
+MOV AH, 09h
+LEA DX,msgSelect
+INT 21h
+MOV AH, 01h
+INT 21h       
+
+
+
+
+;;;;;;; PROCEMIENTOS ;;;;;;;;
+
+
+;NUEVA LINEA: Este procedimiento realiza un salto de linea           
+nwLine PROC
+MOV AH,02h
+MOV DX,0Ah; imprime asscii 10 (nueva linea)
+INT 21h
+MOV DX,0Dh; imprime asscii 13 (retorno de carro)
+INT 21h
+RET
+nwLine ENDP            
+         
+;;;;;;; FIN PROCEMIENTOS ;;;;;;;;         
+          
+          
+          
